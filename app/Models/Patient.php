4dc6b74eac07e91d8c9008646ee04a3e378916f7
julien <zigan.julien@gmail.com>
2025-12-19 12:57:45 +0000

    api: add patient endpoints

---
+++ app/Models/Patient.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    /** @use HasFactory<\Database\Factories\PatientFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'date_of_birth',
+        'email',
+        'phone',
+        'address',
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TherapistController;
+use App\Http\Controllers\Api\PatientController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('therapists', TherapistController::class);
+Route::apiResource('patients', PatientController::class);
