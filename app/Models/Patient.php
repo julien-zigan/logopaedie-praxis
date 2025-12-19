555e89633c7bf637f11b3ab574c394556019acd1
julien <zigan.julien@gmail.com>
2025-12-19 14:50:01 +0000

    api: add therapy session endpoints

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Patient extends Model
 {
@@ -22,4 +23,9 @@ class Patient extends Model
         'phone',
         'address',
     ];
+
+    public function therapySessions(): HasMany
+    {
+        return $this->hasMany(TherapySessions::class);
+    }
 }
--- app/Models/Therapist.php
+++ app/Models/Therapist.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Therapist extends Model
 {
@@ -21,4 +22,9 @@ class Therapist extends Model
         'email',
         'phone',
     ];
+
+    public function therapySessions(): HasMany
+    {
+        return $this->hasMany(TherapySessions::class);
+    }
 }
